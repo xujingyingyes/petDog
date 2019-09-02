70613fb6a739fa66025c8838a45d2f5332ddc559
wzc0x0 <wzc0x0@gmail.com>
2019-09-02 19:22:12 +0800

    兼容h5 标签 + 导航栏样式重新

--- serve.js
+++ serve.js
@@ -1,17 +1,26 @@
-var StaticServer = require('static-server');
-var server = new StaticServer({
-  rootPath: '.',            // required, the root of the server file tree
-  name: 'web-http-server',   // optional, will set "X-Powered-by" HTTP header
-  port: 9000,               // optional, defaults to a random port
-  host: '127.0.0.1',       // optional, defaults to any interface
-  cors: '*',                 // optional, defaults to undefined,
-  followSymlink: true,      // optional, defaults to a 404 error
-  templates: {
-    index: 'index.html',      // optional, defaults to 'index.html'
-    notFound: '404.html'    // optional, defaults to undefined
-  }
+/**
+ * @description gbk decode by python server
+ */
+
+var os = require("os");
+var dns = require("dns");
+var exec = require("child_process").exec;
+
+dns.lookup(os.hostname(), function(err, add, fam) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(`Server on http://localhost:8080/dist`);
+    console.log(`Server on http://${add}:8080/dist`);
 });
 
-server.start(function () {
-  console.log('Server listening to', server.port);
+exec("python -m SimpleHTTPServer 8080", function(err, stdout, stderr) {
+    console.clear();
+    if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
 });
